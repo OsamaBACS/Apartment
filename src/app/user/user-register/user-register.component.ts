00dc8123d85cb6a61287f23f2469222b146e54f6
OsamaBACS <osama.solution@gmail.com>
2021-09-18 16:55:17 +0300

    AddUsersToLocalStorage

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
@@ -8,28 +14,85 @@ import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators }
 })
 export class UserRegisterComponent implements OnInit {
   registrationForm!: FormGroup;
+  user: any = {};
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.registrationForm = new FormGroup({
-      userName: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(8),
-      ]),
-      confirmPassword: new FormControl(null, [Validators.required]),
-      mobile: new FormControl(null, [
-        Validators.required,
-        Validators.maxLength(10),
-      ]),
-    }, this.passwordMatchingValidator);
+    this.createRegistrationForm();
   }
 
-  passwordMatchingValidator(fc: AbstractControl): ValidationErrors | null {
-    return fc.get('password')?.value === fc.get('confirmPassword')?.value ? null : {notMatched: true};
+  createRegistrationForm() {
+    this.registrationForm = this.fb.group(
+      {
+        userName: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        password: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(8),
+        ]),
+        confirmPassword: new FormControl(null, [Validators.required]),
+        mobile: new FormControl(null, [
+          Validators.required,
+          Validators.maxLength(10),
+        ]),
+      },
+      {
+        Validator: this.passwordMatchingValidator(
+          'password',
+          'confirmPassword'
+        ),
+      }
+    );
   }
 
-  onSubmit() {}
+  passwordMatchingValidator(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      console.log(controlName + '--' + matchingControlName);
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (
+        matchingControl.errors &&
+        !matchingControl.errors.confirmedValidator
+      ) {
+        return;
+      }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ confirmedValidator: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
+
+  //----------------------------------------
+  // Getters Methods for all formCOntrols //
+  //----------------------------------------
+  get UserName() {
+    return this.registrationForm.get('userName') as FormControl;
+  }
+  get Email() {
+    return this.registrationForm.get('email') as FormControl;
+  }
+  get Password() {
+    return this.registrationForm.get('password') as FormControl;
+  }
+  get ConfirmPassword() {
+    return this.registrationForm.get('confirmPassword') as FormControl;
+  }
+  get Mobile() {
+    return this.registrationForm.get('mobile') as FormControl;
+  }
+  //----------------------------------------
+
+  onSubmit() {
+    this.user = Object.assign(this.user, this.registrationForm.value);
+    this.addUser(this.user);
+  }
+
+  addUser(user: any) {
+    localStorage.setItem('Users', JSON.stringify(user));
+  }
 }
