0adf3712e2e803ba9649d8651f0035fe12159503
Osama Aldajan <osama.solution@gmail.com>
2021-10-04 14:55:04 +0300

    AddMultiplePropertiestoLocalStorage

--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { IProperty } from '../model/iproperty';
+import { Property } from '../model/property';
 
 @Injectable({
   providedIn: 'root',
@@ -14,6 +15,15 @@ export class HousingService {
     return this.http.get('data/properties.json').pipe(
       map((data: any) => {
         const propertiesArray: Array<IProperty> = [];
+        const localProperties = JSON.parse(localStorage.getItem('newProp')!);
+        if(localProperties){
+          for (var id in localProperties) {
+            if (localProperties.hasOwnProperty(id) && localProperties[id].SellRent === SellRent) {
+              propertiesArray.push(localProperties[id]);
+            }
+          }
+        }
+
         for (var id in data) {
           if (data.hasOwnProperty(id) && data[id].SellRent === SellRent) {
             propertiesArray.push(data[id]);
@@ -22,5 +32,22 @@ export class HousingService {
         return propertiesArray;
       })
     );
+  }
+  addProperty(property: Property) {
+    let newProp = [property];
+    localStorage.setItem('newProp', JSON.stringify(newProp));
+    if(localStorage.getItem('newProp')){
+      newProp = [property, ...JSON.parse(localStorage.getItem('newProp')!)];
+    }
+  }
+
+  newPropId(){
+    if(localStorage.getItem('PID')){
+      localStorage.setItem('PID', String(+localStorage.getItem('PID')! + 1));
+      return +localStorage.getItem('PID')!;
+    } else {
+      localStorage.setItem('PID', '101');
+      return 101;
+    }
   }
 }
